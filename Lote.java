c45908fbe09b9134327caa4a970907ba21fa54ee
Oscar Huanca <oscarhuank@gmail.com>
2023-08-30 10:08:45 -0400

    version 1

---
+++ Lote.java
@@ -0,0 +1,58 @@
+
+public class Lote
+{
+    private int numeroDeLote;
+    private String descripcion;
+    private Oferta ofertaMaxima;
+    
+    public Lote(int numeroDeLote, String descripcion)
+    {
+        this.numeroDeLote = numeroDeLote;
+        this.descripcion = descripcion;
+    }
+    
+    public boolean ofertarPara(Oferta oferta)
+    {
+        if (( ofertaMaxima == null) || (oferta.getMonto() >  ofertaMaxima.getMonto()))
+        {
+            ofertaMaxima = oferta;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    public Oferta getOfertaMaxima()
+    {
+        return ofertaMaxima;
+    }
+    
+     public int getNumero()
+    {
+        return numeroDeLote;
+    }
+    
+    public String getDescripcion()
+    {
+        return descripcion;
+    }
+    
+    public String toString()
+    {
+        String cadena = numeroDeLote + ": " + descripcion;
+        if(ofertaMaxima != null) cadena += "  oferta max: " +    ofertaMaxima.getMonto();
+        return cadena;
+    }
+    
+    public String mostrarGanador()
+    {
+        String cadena = numeroDeLote + ": " +descripcion;
+        
+        cadena += " Ganador :" + ofertaMaxima.getNombre();
+        cadena += " Monto:   " + ofertaMaxima.getMonto();
+        return cadena;
+    }
+    
+}
---
+++ Persona.java
@@ -0,0 +1,19 @@
+
+public class Persona
+{
+    private String nombre;
+    
+    public Persona(String nombre)
+    {
+        this.nombre = nombre;
+    }
+    
+    public void setPersona(String nombre)
+    {
+        this.nombre = nombre;
+    }
+    public String getPersona()
+    {
+        return nombre;
+    }
+}
